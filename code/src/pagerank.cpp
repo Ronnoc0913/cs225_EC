bbb053fbda340f08ee48bf3937991693a10a2375
ConnorT13 <connor.tan@gmail.com>
2023-12-04 00:01:23 +0000

    wrote page rank calculation algo

--- code/src/pagerank.cpp
+++ code/src/pagerank.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <cmath>
 
 vector<vector<int>> createAdjacencyMatrix(const string &filename, int numNodes)
 {
@@ -41,4 +42,50 @@ vector<vector<double>> createTransitionMatrix(const vector<vector<int>>& adjacen
     }
 
     return transitionMatrix;
+}
+
+/*Function to calculate the PageRank of nodes in a graph
+  TransitionMatrix: transition probability matrix of graph
+  numIterations: maximum number of iterations to run the algorithm.
+  dampFactor: damping factor */
+vector<double> calculatePageRank(const vector<vector<double>>& transitionMatrix, int numIterations, double dampFactor){
+    int numNodes = transitionMatrix.size(); 
+    vector<double> pageRank(numNodes, 1.0 / numNodes);
+    vector<double> newPageRank(numNodes, 0.0);
+
+    for(int iter = 0; iter < numIterations; ++iter){
+        double danglingRank = 0.0;
+        //calculate total rank contribution from dangling nodes
+        for(int i = 0; i < numNodes; ++i){
+            // sum of probablilities in current row 
+            double rowSum = std::accumulate(transitionMatrix[i].begin(), transitionMatrix[i].end(), 0.0);
+            // if sum is zero, there is a dangling node
+            if(rowSum == 0){
+                danglingRank += pageRank[i] / numNodes;
+            }
+        }
+        //calculate new pagerank value for each node
+        for(int i = 0; i < numNodes; ++i){
+            //teleportation contribution
+            newPageRank[i] = (1.0 - dampFactor)/ numNodes;
+            //add the rank contributions from other nodes
+            for(int j = 0; j < numNodes; ++j){
+                newPageRank[i] += dampFactor * transitionMatrix[j][i] * pageRank [j];
+            }
+            // Add the contribution from dangling nodes.
+            newPageRank[i] += danglingRank * dampFactor;
+        }
+
+        double diff = 0.0;
+        for(int i = 0; i < numNodes; ++i){
+            diff += std::abs(newPageRank[i] - pageRank[i]);
+
+        }
+        if(diff < 0.001){
+            break;
+        }
+        pageRank = newPageRank;
+
+    }
+    return pageRank;
 }
--- code/src/pagerank.h
+++ code/src/pagerank.h
@@ -2,9 +2,13 @@
 
 #include <fstream>
 #include <vector>
+#include <cmath>
+#include <numeric>
 
 using namespace std;
 
 vector<vector<int>> createAdjacencyMatrix(const string& filename, int numNodes);
 
 vector<vector<double>> createTransitionMatrix(const vector<vector<int>>& adjacencyMatrix);
+
+vector<double> calculatePageRank(const vector<vector<double>>& transitionMatrix, int numIterations, double dampFactor = 0.85);
